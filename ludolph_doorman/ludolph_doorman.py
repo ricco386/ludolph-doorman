247f7c469846c900ddbed53aa4873affd764b86b
Richard Kellner <richard@kellnerovci.sk>
2016-01-16 10:50:03 +0100

    Updated plugin to output door_state directly from doorman library

--- ludolph_doorman/ludolph_doorman.py
+++ ludolph_doorman/ludolph_doorman.py
@@ -6,7 +6,7 @@ See the LICENSE file for copying permission.
 import time
 from doorman.doorman import Doorman
 
-#from ludolph_doorman import __version__
+from ludolph_doorman import __version__
 from ludolph.command import CommandError, command
 from ludolph.plugins.plugin import LudolphPlugin
 
@@ -15,15 +15,15 @@ class LudolphDoorman(LudolphPlugin):
     """
     Ludolph: Doorman, plugin commands.
     """
-#    __version__ = __version__
+    __version__ = __version__
 
     # noinspection PyUnusedLocal
     @command
     def door_state(self, msg):
         """
-        Door state.
+        Doorman get output from get_door_state function.
 
         Usage: door-state
         """
         d = Doorman()
-        return 'Door state is %s' % d.door_state
+        return d.get_door_state()
